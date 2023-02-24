4c9de8838245fc2c6df0cefc6650ef9ca818e1b6
Mark Wördenweber <mark.woerdenweber@smartstore.com>
2023-02-24 13:26:22 +0100

    Removed HelloWorldTabs (Now: TabsTutorial)

--- src/MyOrg.HelloWorldTabs/Configuration/HelloWorldSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using Smartstore.Core.Configuration;
-
-namespace MyOrg.HelloWorld.Settings
-{
-    public class HelloWorldSettings : ISettings
-    {
-        public string Name { get; set; } = "John Smith";
-    }
-}
--- src/MyOrg.HelloWorldTabs/Models/PublicInfoModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Smartstore.Web.Modelling;
-
-namespace MyOrg.HelloWorld.Models
-{
-    public class PublicInfoModel : ModelBase
-    {
-        public string Name { get; set; }
-    }
-}
